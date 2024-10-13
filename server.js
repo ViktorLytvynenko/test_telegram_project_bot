a98183950a90d2c659272659433eebd3ed904e39
Viktor Lytvynenko <lytvynenkobooks@gmail.com>
2024-10-13 14:22:30 +0300

    fixes

--- server.js
+++ server.js
@@ -1,38 +1,22 @@
-import express from 'express';
-import { Telegraf } from 'telegraf';
-import dotenv from 'dotenv';
-
-dotenv.config();
+import {Telegraf} from 'telegraf';
 
 const bot = new Telegraf(process.env.TOKEN);
-const app = express();
-const PORT = process.env.PORT || 3000;
 
 bot.start((ctx) => {
     const webAppButton = {
         text: 'Open Web App',
-        web_app: { url: 'https://test-telegram-project-frontend.vercel.app/' }
+        web_app: {url: 'https://test-telegram-project-frontend.vercel.app/'}
     };
 
     const keyboard = {
         inline_keyboard: [[webAppButton]]
     };
 
-    ctx.reply('Welcome! Click me!', {
+    ctx.reply("Welcome! Click me!", {
         reply_markup: keyboard
     });
 });
 
-const webhookPath = '/telegram-webhook';
-const webhookUrl = `https://test-telegram-project-frontend.vercel.app/${webhookPath}`;
-
-bot.telegram.setWebhook(webhookUrl);
-app.use(bot.webhookCallback(webhookPath));
-
-app.get('/', (req, res) => {
-    res.send('Telegram Bot Backend is running!');
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+bot.launch()
+    .then(() => console.log('Bot is running...'))
+    .catch(err => console.error('Failed to launch the bot:', err));
